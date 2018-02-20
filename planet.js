d2d228b2b2246880c2a89061901981bc61fb7cf6
Peter Horton <phorton@softstackfactory.org>
2018-02-20 04:06:57 +0000

    created the planet.ts, transpile it to js

---
+++ planet.js
@@ -0,0 +1,18 @@
+var Planet = /** @class */ (function () {
+    function Planet(name, radius, hasRing, color) {
+        this.name = name;
+        this.radius = radius;
+        this.hasRing = hasRing;
+        this.color = color;
+    }
+    Planet.prototype.printPlanet = function () {
+        console.log("Planet " + this.name + " ,Radius: " + this.radius + " ,Has Ring: " + this.hasRing + " ,Color: " + this.color);
+    };
+    return Planet;
+}());
+var Mercury = new Planet("Mercury", 10, false, "yellow");
+var Venus = new Planet("Venus", 20, false, "gray");
+var Saturn = new Planet("Saturn", 60, true, "rainbow");
+Mercury.printPlanet();
+Venus.printPlanet();
+Saturn.printPlanet();
---
+++ planet.ts
@@ -0,0 +1,26 @@
+class Planet {
+    name : string;
+    radius : number;
+    hasRing : boolean;
+    color: string
+    
+    constructor(name:string, radius:number, hasRing:boolean, color:string){
+        this.name = name;
+        this.radius = radius;
+        this.hasRing = hasRing;
+        this.color = color;
+    }
+    printPlanet(){
+        console.log("Planet " + this.name + ", Radius: " + this.radius + ", Has Ring: " + this.hasRing + ", Color: " + this.color );
+    }
+    
+}
+
+let Mercury = new Planet("Mercury", 10, false, "yellow");
+let Venus = new Planet("Venus", 20, false, "gray");
+let Saturn = new Planet("Saturn", 60, true, "rainbow");
+
+Mercury.printPlanet();
+Venus.printPlanet();
+Saturn.printPlanet();
+
